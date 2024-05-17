532697073020a1394c79c1c59b7f7bd154f155dd
bmyoungblood <bmyoungblood94@gmail.com>
2024-05-17 18:13:36 -0500

    created agent class

--- lab_9b.py
+++ lab_9b.py
@@ -7,5 +7,29 @@ Created on Fri May 17 17:43:10 2024
 
 ## Brianna Youngblood 
 
+## Simulation: Schelling Model 
 
+## Create an Agent Class
+
+class Agent:
+    def __init__(self, type):
+        self.type = type
+        self.happy = False
+        pass
+    def happy(self, neighborhood, threshold):
+        same_neighbors= sum(1 for n in neighborhood if n == self.type)
+        total_neighbors= len(neighborhood)
+        if total_neighbors == 0:
+            return False
+        ratio = same_neighbors / total_neighbors
+        return ratio >= threshold
+    
+
+
+
+## Create a World Class
+
+## Initialize
+
+## Create Loop 
 
